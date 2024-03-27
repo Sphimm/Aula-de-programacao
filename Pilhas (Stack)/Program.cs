9ca0433b057113ab1ee47e3a2bed92a0a0af9c2f
Sphimm <160131574+Sphimm@users.noreply.github.com>
2024-03-27 11:12:34 -0300

    Add exercício de Stacks

---
+++ Pilhas (Stack)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Pilhas__Stack_;
+
+Console.WriteLine("Selecione um dos itens abaixo:");
+
+Console.WriteLine();
+
+Console.WriteLine("1 - Criar Pilha");
+Console.WriteLine("2 - Adicionar item à pilha");
+Console.WriteLine("2 - Adicionar item à pilha");
+
+int opcao = int.Parse(Console.ReadLine());
+Stack Pilha = new Stack();
+
+if (opcao == 0)
+{
+    Pilha.Criar_Stack();
+}
---
+++ Pilhas (Stack)/pilha.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilhas__Stack_
+{
+    public class Stack
+    {
+        Stack<string> pilha {  get; set; }
+
+        public void Criar_Stack()
+        {
+        pilha = new Stack<string>();
+        }
+
+        public void Add_Stack(string item)
+        {
+            pilha.Push(item);
+        }
+
+        public void Clear_Stack()
+        {
+            pilha.Clear();
+        }
+
+        public void Spy_Stack()
+        {
+            pilha.Peek();
+        }
+
+        public void View_Stack()
+        {
+            pilha.();
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
