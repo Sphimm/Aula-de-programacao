5472bfafb50289948a313025c5cbfa6d5c48997f
Sphimm <160131574+Sphimm@users.noreply.github.com>
2024-04-10 10:58:09 -0300

    Update Filas
    Nome da solução atualizado

--- Filas (Vetor)/Program.cs
+++ Filas (Vetor)/Program.cs
@@ -1,64 +1,18 @@
 ﻿using Filas__Vetor_;
 
 Vetor fila = new Vetor();
-Interface UI = new Interface();
-bool verf = true;
 string? item;
-int quant;
-string quant_S;
-int opcao;
-string opcao_S;
 
-while (verf)
-{
-    UI.Divisao_Longa();
-    UI.Inicio();
-    UI.Divisao_Longa();
+Console.WriteLine("Adicione um item na fila:");
+item = Console.ReadLine();
+fila.Adicionar(item);
 
-    UI.Insira_Aqui();
-    opcao_S = Console.ReadLine();
-    Console.WriteLine();
+Console.WriteLine("Adicione um item na fila:");
+item = Console.ReadLine();
+fila.Adicionar(item);
 
-    if (opcao == 1)
-    {
-        Console.WriteLine("Escreva a quantidade de lugares na fila:");        
-        Console.WriteLine();
-        UI.Insira_Aqui();
-        quant_S = Console.ReadLine();
-        quant = UI.Verificar(quant_S);
-        fila.Criar(quant);
-    } //Criar Fila
+fila.Spy();
 
-    else if (opcao == 2)
-    {
-        Console.WriteLine("Escreva o item a ser adicionado:");
-        Console.WriteLine();
-        UI.Insira_Aqui();
-        item = Console.ReadLine();
-        fila.Adicionar(item);
-    } //Adicionar Item
+fila.Remover();
 
-    else if (opcao == 3)
-    {
-        fila.Remover();
-    } //Excluir Item
-
-    else if (opcao == 4)
-    {
-        fila.Spy();
-    } //Ver Item no Topo
-
-    else if (opcao == 5)
-    {
-        fila.View();
-    } //Ver Todos os Itens
-
-    else
-    {
-        verf = false;
-        for (int i = 0; i < 100; i++)
-        {
-            Console.Write("=");
-        }
-    } //Encerrar
-}
+fila.Spy();
--- Filas (Vetor)/Vetor.cs
+++ Filas (Vetor)/Vetor.cs
@@ -8,108 +8,36 @@ namespace Filas__Vetor_
 {
     public class Vetor
     {
-        string[]? fila;
+        string[] fila = new string[5];
         int indice = 0;
-
-        public void Criar(int add)
-        {
-            Console.WriteLine();
-            fila = new string[add];
-            Console.WriteLine($"Fila criada com {add} espaços");
-            Console.WriteLine();
-        }
-
         public void Adicionar(string? add)
         {
-            if (indice < fila.Length)
-            {
-                fila[indice] = add;
-                indice++;
-                Console.WriteLine();
-                Console.WriteLine($"O item: '{add}' foi adicionado");
-                Console.WriteLine();
-            }
-            
-            else
-            {
-                Console.WriteLine("A fila está cheia");
-                Console.WriteLine();
-                return;
-            }
+            fila[indice] = add;
+            indice++;
+            Console.WriteLine();
+            Console.WriteLine($"O item: '{add}' foi adicionado");
+            Console.WriteLine();
         }
 
         public void Remover()
         {
-            if (indice > 0)
-            {
-                Console.WriteLine();
-                Console.WriteLine($"O item: '{fila[0]}' foi removido");
-                fila[0] = null;
-                indice--;
-                Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"O item: '{fila[0]}' foi removido");
+            fila[0] = null;            
+            Console.WriteLine();
 
-                for (int i = 0; i < indice; i++)
-                {
-                    if (indice > 0)
-                    {
-                        fila[i] = fila[i + 1];
-                        fila[i + 1] = null;
-                    }
-                    else
-                    {
-                        return;
-                    }
-                }
-            }
-            
-            else
+            for (int i = 0; i < indice; i++)
             {
-                Console.WriteLine("A fila está vazia");
-                Console.WriteLine();
-                return;
+                fila[i] = fila[i + 1];
+                fila[i + 1] = null;
             }
         }
 
         public void Spy()
         {
-            if (indice > 0)
-            {
-                Console.WriteLine();
-                Console.WriteLine($"O primeiro item da fila é: '{fila[0]}'");
-                Console.WriteLine();
-            }            
-            else
-            {               
-                Console.WriteLine("A fila está vazia");
-                Console.WriteLine();
-                return;
-            }
-        }
-
-        public void View()
-        {
-            if (indice > 0)
-            {
-                Console.WriteLine();
-                Console.WriteLine("Os itens da fila são:");
-                Console.WriteLine();
-
-                foreach (string? item in fila)
-                {
-                    if (item != null)
-                    {
-                        Console.WriteLine(item);
-                        Console.WriteLine();
-                    }
-                }
-            }
-            
-            else
-            {
-                Console.WriteLine("A fila está vazia");
-                Console.WriteLine();
-                return;
-            }
+            Console.WriteLine();
+            Console.WriteLine($"O primeiro item da fila é: '{fila[0]}'");
+            Console.WriteLine();
         }
     }
 }
