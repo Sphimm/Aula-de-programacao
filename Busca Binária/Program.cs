b8f2bbda104a53845d78c03030af331450395f9f
Sphimm <160131574+Sphimm@users.noreply.github.com>
2024-04-10 11:02:52 -0300

    Add Busca Binária e Linear

---
+++ Busca Binária/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("Qual o tamanho da lista?");
+int tamanho = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int[] lista = new int[tamanho];
+
+int inicio = 0; 
+int meio; 
+int fim = lista.Length - 1;
+int procurado;
+
+for (int i = 0; i < lista.Length; i++)
+{
+    Console.WriteLine($"Digite o {i + 1}º número: ");
+    lista[i] = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+}
+for (int i = 0; i < lista.Length - 1; i++)
+{
+    for (int j = 0; j < lista.Length - 1 - i; j++)
+    {
+        if (lista[j] > lista[j + 1])
+        {
+            int temp = lista[j];
+            lista[j] = lista[j + 1];
+            lista[j + 1] = temp;
+        }
+    }
+}
+
+Console.WriteLine("Digite o número que deseja procurar: ");
+Console.WriteLine();
+procurado = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+do
+{
+    meio = (inicio + fim) / 2;
+    if (lista[meio] == procurado)
+    {
+        Console.WriteLine($"O número {procurado} foi encontrado na posição: {meio + 1} no índice {meio}");
+        Console.WriteLine();
+        break;
+    }
+    
+    else if (lista[meio] < procurado)
+    {
+        inicio = meio + 1;
+    }
+    
+    else
+    {
+        fim = meio - 1;
+    }
+} while (inicio <= fim);
---
+++ Busca Linear/Program.cs
@@ -0,0 +1,34 @@
+﻿string?[] nomes = new string[5];
+string? nomeBusca;
+int count = 0;
+
+for (int i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine($"Escreva o nome da posição: {i + 1}");
+    Console.WriteLine();
+    nomes[i] = Console.ReadLine();
+    Console.WriteLine();
+}
+
+Console.WriteLine("Escreva o nome para ser buscado:");
+Console.WriteLine();
+nomeBusca = Console.ReadLine();
+Console.WriteLine();
+
+foreach (string nome in nomes)
+{
+    count++;
+
+    if (nome.Equals(nomeBusca))
+    {
+        Console.WriteLine($"Nome: {nome}, encontrado na posição {count}");
+        Console.WriteLine();
+        break;
+    }
+
+    else
+    {
+        Console.WriteLine($"Nome não encontrado, nome atual: {nome}, na posição {count}");
+        Console.WriteLine();
+    }
+}
