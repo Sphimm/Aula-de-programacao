2c249920f6e44b397a8ca1842ac296c7be817eb7
Sphimm <160131574+Sphimm@users.noreply.github.com>
2024-08-16 11:20:05 -0300

    Encapsulamento

---
+++ Encapsulamento/Encapsulamento/Conta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    public class Conta
+    {
+        private string? cpf;
+        private string? nome;
+        private double saldo;
+
+        public Conta(string cpf, string nome, double saldo)
+        {
+            this.cpf = cpf;
+            this.nome = nome;
+            this.saldo = saldo;
+        }
+
+        public string Cpf { get { return cpf; } }
+        public string Nome { get { return nome; } set { nome = value; } }
+        public double Saldo { get { return saldo; } set { saldo = value; } }
+    }
+}
---
+++ Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,3 @@
+﻿using Encapsulamento;
+
+Conta c = new Conta("123.456.789-00", "Fulano", 1000);
